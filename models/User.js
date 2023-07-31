0f962e63628037f348181b3e226c043a4b6bfba6
wontonchris <127700194+wontonchris@users.noreply.github.com>
2023-07-30 19:19:30 -0500

    still working lol

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: 'You need to provide a username!',
+        trim: true
+    },
+    email: {
+        type: String,
+        required: 'You need to provide an email address!',
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+},
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+}); 
+
+
+const User = model('User', userSchema);
+module.exports = User;
--- models/index.js
+++ models/index.js
@@ -1,4 +1,3 @@
-const Student = require('./Student');
-const Course = require('./Course');
 
-module.exports = { Student, Course };
+
+module.exports = { };
