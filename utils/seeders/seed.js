365bb9b35ececa753d6d03c8eccadd2328ca8831
wontonchris <127700194+wontonchris@users.noreply.github.com>
2023-08-10 12:23:23 -0500

    wtf

--- utils/seeders/seed.js
+++ utils/seeders/seed.js
@@ -49,34 +49,64 @@ connection.once('open', async () => {
     const thoughtText = thoughtText[i];
     const userName = userName[i];
 
-    // Get some random assignment objects using a helper function that we imported from ./data
-    const assignments = getRandomAssignments(20);
+    // // Get some random assignment objects using a helper function that we imported from ./data
+    // const assignments = getRandomAssignments(20);
 
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
-    const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
+    // const fullName = getRandomName();
+    // const first = fullName.split(' ')[0];
+    // const last = fullName.split(' ')[1];
+    // const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
 
-    students.push({
-      first,
-      last,
-      github,
-      assignments,
+    thoughts.push({
+      thoughtText,
+      userName,
     });
   }
 
   // Add students to the collection and await the results
-  await Student.collection.insertMany(students);
+  await Thought.collection.insertMany(thoughts);
 
+  //Seed Email for users
+  const email = [
+    'ByronDaTeacher@gmail.com',
+    'ChrisTheTA@gmail.com,',
+    'JoshDaGoat@gmail.com',
+    'CollinIsBallin@gmail.com',
+    'LebroncoTuesdays@gmail.com',
+
+  ];
+
+  // use the new thought ID to add to the user
+  const getAllThoughts = await Thought.find({});
+  const thoughtIDs = getAllThoughts.map((thought) => thought._id);
+  const users = [];
+
+  // add seed user info to array
+  for (let i = 0; i < 5; i++) {
+    const userName = userName[i];
+    const email = email[i]; 
+    const thoughts = thoughtIDs[i];
+
+    users.push({
+      userName,
+      email,
+      thoughts,
+    });
+  }
+
+
+
+  
   // Add courses to the collection and await the results
-  await Course.collection.insertOne({
-    courseName: 'UCLA',
-    inPerson: false,
-    students: [...students],
-  });
+  await User.collection.insertMany(users));
+//     courseName: 'UCLA',
+//     inPerson: false,
+//     students: [...students],
+//   });
 
-  // Log out the seed data to indicate what should appear in the database
-  console.table(students);
+//   // Log out the seed data to indicate what should appear in the database
+  console.table(thoughts);
+  console.table(users);
   console.info('Seeding complete! 🌱');
   process.exit(0);
 });
