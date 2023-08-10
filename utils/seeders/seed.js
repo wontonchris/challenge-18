6c301b8c6644c658865adf821aeeb99c1f7066cb
wontonchris <127700194+wontonchris@users.noreply.github.com>
2023-08-10 12:44:48 -0500

    seed.js

--- utils/seeders/seed.js
+++ utils/seeders/seed.js
@@ -1,23 +1,22 @@
-const connection = require('../config/connection');
-const { Thought, User } = require('../models');
+const connection = require('../../config/connection');
+const { Thought, User } = require('../../models');
+
 
 connection.on('error', (err) => err);
 
 connection.once('open', async () => {
   console.log('connected');
-    // Delete the collections if they exist
-    let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
-    if (thoughtCheck.length) {
-      await connection.dropCollection('thoughts');
-    }
+  // Delete the collections if they exist
+  let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
+  if (thoughtCheck.length) {
+    await connection.dropCollection('thoughts');
+  }
 
-    let usersCheck = await connection.db.listCollections({ name: 'users' }).toArray();
-    if (usersCheck.length) {
-      await connection.dropCollection('users');
-    }
+  let usersCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+  if (usersCheck.length) {
+    await connection.dropCollection('users');
+  }
 
-
-  // Create empty array to hold the students
   const thoughts = [];
 
   const thoughtText = [
@@ -31,7 +30,6 @@ connection.once('open', async () => {
     'I am so sad',
     'I am so lonely',
     'Lebron is the GOAT',
-
   ];
 
   const userName = [
@@ -40,71 +38,49 @@ connection.once('open', async () => {
     'JoshDaGoat',
     'CollinIsBallin',
     'LebroncoTuesdays',
-
+    'KobeIsTheGoat',
+    'MJIsTheGoat',
+    'KareemIsTheGoat',
+    'WiltIsTheGoat',
+    'BillRussellIsTheGoat',
   ];
 
-
-  // Loop 20 times -- add students to the students array
-  for (let i = 0; i < 20; i++) {
-    const thoughtText = thoughtText[i];
-    const userName = userName[i];
-
-    // // Get some random assignment objects using a helper function that we imported from ./data
-    // const assignments = getRandomAssignments(20);
-
-    // const fullName = getRandomName();
-    // const first = fullName.split(' ')[0];
-    // const last = fullName.split(' ')[1];
-    // const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
-
+  // Loop 20 times -- add thoughts to the thoughts array
+  for (let i = 0; i < 10; i++) {
     thoughts.push({
-      thoughtText,
-      userName,
+      thoughtText: thoughtText[i],
+      userName: userName[i],
     });
   }
 
-  // Add students to the collection and await the results
+  // Add thoughts to the collection and await the results
   await Thought.collection.insertMany(thoughts);
 
-  //Seed Email for users
+  // Seed Email for users
   const email = [
     'ByronDaTeacher@gmail.com',
-    'ChrisTheTA@gmail.com,',
+    'ChrisTheTA@gmail.com',
     'JoshDaGoat@gmail.com',
     'CollinIsBallin@gmail.com',
     'LebroncoTuesdays@gmail.com',
-
   ];
 
-  // use the new thought ID to add to the user
   const getAllThoughts = await Thought.find({});
   const thoughtIDs = getAllThoughts.map((thought) => thought._id);
   const users = [];
 
-  // add seed user info to array
+  // Add seed user info to array
   for (let i = 0; i < 5; i++) {
-    const userName = userName[i];
-    const email = email[i]; 
-    const thoughts = thoughtIDs[i];
-
     users.push({
-      userName,
-      email,
-      thoughts,
+      userName: userName[i],
+      email: email[i],
+      thoughts: [thoughtIDs[i]],
     });
   }
 
+  // Add users to the collection and await the results
+  await User.collection.insertMany(users);
 
-
-  
-  // Add courses to the collection and await the results
-  await User.collection.insertMany(users));
-//     courseName: 'UCLA',
-//     inPerson: false,
-//     students: [...students],
-//   });
-
-//   // Log out the seed data to indicate what should appear in the database
   console.table(thoughts);
   console.table(users);
   console.info('Seeding complete! 🌱');
